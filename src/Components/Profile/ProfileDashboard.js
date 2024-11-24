72b045c0336c1aa3af1b0f4b4b1d78786f69545a
Bala <malharikingdom9@gmail.com>
2024-11-24 11:22:17 +0530

    added react

--- src/Components/Profile/ProfileDashboard.js
+++ src/Components/Profile/ProfileDashboard.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { Grid, Card, CardContent, Typography, Box } from "@mui/material";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
