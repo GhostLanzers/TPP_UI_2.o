81c0a3433cea398fd3049ea437894d8206457dfe
Bala <malharikingdom9@gmail.com>
2026-01-27 22:45:57 +0530

    Axios Fix

--- src/Components/Main/AxiosInstance.js
+++ src/Components/Main/AxiosInstance.js
@@ -7,7 +7,7 @@ import axios from "axios";
 // };
 
 const AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: process.env?.REACT_APP_API_BASE_URL,
    headers: {
       "Content-Type": "application/json",
    },
